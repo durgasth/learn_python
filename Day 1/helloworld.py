167dc78f0474858517248a486a076eec3cdb25c0
durgasth <durgasth2005@gmail.com>
2025-07-05 22:45:53 +0545

    additional subtraction and multiplication

--- Day 1/helloworld.py
+++ Day 1/helloworld.py
@@ -10,8 +10,13 @@ print(3 // 2)  # Floor division operator(//)
 
 a = 2
 b = 3
-
 print("The addition of a and b :", a+b)
 
+a = 2
+b = 3
+print("The subtraction of a and b :", a - b)
 
+a = 2
+b = 3
+print("The multiplication of a and b :" , a*b)
 
