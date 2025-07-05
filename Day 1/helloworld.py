de3fc3b59123b8782f2579b391cff4eae87079f4
durgasth <durgasth2005@gmail.com>
2025-07-05 15:42:43 +0545

    day 1 of learning python

---
+++ Day 1/helloworld.py
@@ -0,0 +1,17 @@
+print("Hello World")
+print(3 + 2)   # addition(+)
+print(3 - 2)   # subtraction(-)
+print(3 * 2)   # multiplication(*)
+
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+a = 2
+b = 3
+
+print("The addition of a and b :", a+b)
+
+
+
