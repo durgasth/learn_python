02190b8bfca49949f5e935c2922f0016cfee7b3e
durgasth <durgasth2005@gmail.com>
2025-07-06 12:53:11 +0545

    assignment

--- Assignment/day1assignment.py
+++ Assignment/day1assignment.py
@@ -1,1 +1,11 @@
-# Asignment 
+a = 2
+b = 3
+print("The addition of a and b :", a+b)
+
+a = 2
+b = 3
+print("The subtraction of a and b :", a - b)
+
+a = 2
+b = 3
+print("The multiplication of a and b :" , a*b)
