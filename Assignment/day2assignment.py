593eb805486732cb2de90388da393c0cb11e187a
durgasth <durgasth2005@gmail.com>
2025-07-07 19:41:23 +0545

    Number Sequence

--- Assignment/day2assignment.py
+++ Assignment/day2assignment.py
@@ -13,3 +13,24 @@ print(person)
 
 
 # Number sequence mapping (datatype, data)
+
+# for sequence 
+# List
+list_data = [1, 2, 3, 4, 5]
+print("Datatype: list\t\tData:", list_data)
+
+# Tuple
+tuple_data = (1, 2, 3, 4, 5)
+print("Datatype: tuple\t\tData:", tuple_data)
+
+# String
+str_data = "12345"
+print("Datatype: str\t\tData:", str_data)
+
+# for mapping 
+
+# Dictionary
+dict_data = {1: "one", 2: "two", 3: "three"}
+print("Datatype: dict\t\tData:", dict_data)
+
+
