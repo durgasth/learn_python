fd0f148a0072bd366bc27e2d021bbfe90998e742
durgasth <durgasth2005@gmail.com>
2025-07-10 21:11:38 +0545

    for loop day 5

---
+++ Assignment/day5assignment.py
@@ -0,0 +1,18 @@
+# 1. Print Numbers 1 to 10
+
+# 2. Print the Multiplication Table of a Number
+# Ask the user for a number and print its table (1 to 10).
+# print(f"{num} x {i} = {num * i}")
+
+# 3. Print All Even Numbers from 1 to 20
+
+# 4. Calculate the Sum of Numbers from 1 to N
+
+# 5. Print Each Character of a String
+# word = input("Enter a word: ")
+# Durga
+# D
+# u
+# r
+# g
+# a
---
+++ Day 5/range_function.py
@@ -0,0 +1,36 @@
+# range(5)  #0,1,2,3,4
+# #range(stop)
+
+# range(2,5) #2,3,4
+# # range(start,stop)
+# range(2,10,2) #2,4,6,8   #start,stop,step
+# range (10,2,-2) #10,8,6,4,2   #start,stop,step
+
+# for item in range(5):
+#     #[0,1,2,3,4]
+#     print(item) #output: 0 1 2 3 4
+
+# for i in range(7):
+#     #[0,1,2,3,4,5,6]
+#     print(i) #Output:0 1 2 3 4 5 6
+
+# for i in range (1,7):
+#     #[1,2,3,4,5,6]
+#     print(i )
+
+
+# fruits=["apple","banana","orange"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+# friends=["aastha","punam","smriti"]
+# for item in friends:
+#     print(item)
+
+# for i in range (1,10+1):
+#     print(i)
+
+# range(start, stop, step)
+for i in range(10, 0, -1):
+    print(i)
