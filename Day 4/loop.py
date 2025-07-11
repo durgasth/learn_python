7cbcc2f46ec7815e615647baada0f063b9af6ca2
durgasth <durgasth2005@gmail.com>
2025-07-11 23:09:37 +0545

    day2pratice

--- Day 4/loop.py
+++ Day 4/loop.py
@@ -3,8 +3,8 @@
 # while num > 0:
 #     print(f"Number is possitive: num")
     
-num = int(input("Enter the number: "))
-while num < 10:
-    num += 1 
-    # print("Number is now: {}".format(num))
-    print(f"Number is now: {num}")
+# num = int(input("Enter the number: "))
+# while num < 10:
+#     num += 1 
+#     # print("Number is now: {}".format(num))
+#     print(f"Number is now: {num}")
