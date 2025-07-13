d6e8314d0acef05a9e925a96260d1cfad6571df8
durgasth <durgasth2005@gmail.com>
2025-07-13 19:50:56 +0545

    range

--- Assignment/day5assignment.py
+++ Assignment/day5assignment.py
@@ -1,12 +1,25 @@
 # 1. Print Numbers 1 to 10
+for i in range(1, 11):
+    print(i)
 
 # 2. Print the Multiplication Table of a Number
 # Ask the user for a number and print its table (1 to 10).
 # print(f"{num} x {i} = {num * i}")
 
 # 3. Print All Even Numbers from 1 to 20
+for i in range(2, 21, 2):
+    print(i)
 
 # 4. Calculate the Sum of Numbers from 1 to N
+# Take input from the user
+n = int(input("Enter a number: "))
+
+# Calculate the sum using the formula
+total = sum(range(1, n + 1))
+
+# Display the result
+print(f"The sum of numbers from 1 to {n} is: {total}")
+
 
 # 5. Print Each Character of a String
 # word = input("Enter a word: ")
@@ -16,3 +29,4 @@
 # r
 # g
 # a
+
