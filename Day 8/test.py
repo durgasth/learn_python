4c4a5bd6230097c3fbe208a31154ba1dbca5d082
durgasth <durgasth2005@gmail.com>
2025-07-16 23:01:38 +0545

    day8pratice

--- Day 8/test.py
+++ Day 8/test.py
@@ -1,3 +1,15 @@
-start = ("no" == "yes")
+x = 5
+y = 4
+z = 20.0
 
-print(start)
+
+print("value of x:", x)
+print("vaalue of y:", y)
+
+x, y = y, x
+
+print("value of x:", x)
+print("vaalue of y:", y)
+
+name = "Dristi"
+fruits = ["apple", "mango"]
